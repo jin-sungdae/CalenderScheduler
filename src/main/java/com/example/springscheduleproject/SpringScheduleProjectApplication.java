0e4a80624a6b920bb8b9e0c3b71ebf76fe89cc80
jin-sungdae <makonle@naver.com>
2022-04-01 15:58:34 +0900

    project Repository first

---
+++ src/main/java/com/example/springscheduleproject/SpringScheduleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscheduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springscheduleproject/SpringScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springscheduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
